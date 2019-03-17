d4e276d74cbcf39844c31f8542a65db00c49affe
xxylem <35666594+xxylem@users.noreply.github.com>
2019-03-17 09:21:28 +0800

    Created TokenParser type

--- src/Syntax/Parser.hs
+++ src/Syntax/Parser.hs
@@ -3,51 +3,61 @@ module Syntax.Parser where
 import Data.TokenModel
 import Data.AnalyserModel
 
-parseEIntegerConst :: Token -> Maybe EIntegerConstant
-parseEIntegerConst t =
+type TokenParser a =
+        TokenisedJackFile
+    ->  Maybe (a, TokenisedJackFile)
+
+parseEIntegerConst :: TokenParser EIntegerConstant
+parseEIntegerConst [] = Nothing
+parseEIntegerConst (t:ts) =
     case t of
-        (IC i) -> Just (EIntegerConstant i)
+        (IC i) -> Just (EIntegerConstant i, ts)
         _      -> Nothing
 
-parseEStringConst :: Token -> Maybe EStringConstant
-parseEStringConst t =
+parseEStringConst :: TokenParser EStringConstant
+parseEStringConst [] = Nothing
+parseEStringConst (t:ts) =
     case t of
-        (SC s) -> Just (EStringConstant s)
+        (SC s) -> Just (EStringConstant s, ts)
         _      -> Nothing
 
-parseEKeywordConst :: Token -> Maybe EKeywordConstant
-parseEKeywordConst t =
+parseEKeywordConst :: TokenParser EKeywordConstant
+parseEKeywordConst [] = Nothing
+parseEKeywordConst (t:ts) =
     case t of
-        (KW TrueKW)     -> Just EKConTrue
-        (KW FalseKW)    -> Just EKConFalse
-        (KW Null)       -> Just EKConNull
-        (KW This)       -> Just EKConThis
+        (KW TrueKW)     -> Just (EKConTrue, ts)
+        (KW FalseKW)    -> Just (EKConFalse, ts)
+        (KW Null)       -> Just (EKConNull, ts)
+        (KW This)       -> Just (EKConThis, ts)
         _               -> Nothing
 
-parseEVarName :: Token -> Maybe EVarName
-parseEVarName t =
+parseEVarName :: TokenParser EVarName
+parseEVarName [] = Nothing
+parseEVarName (t:ts) =
     case t of
-        (ID name) -> Just (EVarName name)
+        (ID name) -> Just (EVarName name, ts)
         _         -> Nothing
 
-parseOp :: Token -> Maybe Op
-parseOp t =
+parseOp :: TokenParser Op
+parseOp [] = Nothing
+parseOp (t:ts) =
     case t of
-        (SY Plus)           -> Just OpPlus
-        (SY Minus)          -> Just OpMinus
-        (SY Asterix)        -> Just OpMultiply
-        (SY Slash)          -> Just OpDivide
-        (SY Ampersand)      -> Just OpAnd
-        (SY VerticalBar)    -> Just OpOr
-        (SY LAngleBracket)  -> Just OpLT
-        (SY RAngleBracket)  -> Just OpGT
-        (SY Equal)          -> Just OpEqual
+        (SY Plus)           -> Just (OpPlus, ts)
+        (SY Minus)          -> Just (OpMinus, ts)
+        (SY Asterix)        -> Just (OpMultiply, ts)
+        (SY Slash)          -> Just (OpDivide, ts)
+        (SY Ampersand)      -> Just (OpAnd, ts)
+        (SY VerticalBar)    -> Just (OpOr, ts)
+        (SY LAngleBracket)  -> Just (OpLT, ts)
+        (SY RAngleBracket)  -> Just (OpGT, ts)
+        (SY Equal)          -> Just (OpEqual, ts)
         _                   -> Nothing
 
-parseUnaryOp :: Token -> Maybe UnaryOp
-parseUnaryOp t =
+parseUnaryOp :: TokenParser UnaryOp
+parseUnaryOp [] = Nothing
+parseUnaryOp (t:ts) =
     case t of
-        (SY Minus) -> Just UOPArithNegation
-        (SY Tilde) -> Just UOPBitNegation
+        (SY Minus) -> Just (UOPArithNegation, ts)
+        (SY Tilde) -> Just (UOPBitNegation, ts)
         _          -> Nothing
 
